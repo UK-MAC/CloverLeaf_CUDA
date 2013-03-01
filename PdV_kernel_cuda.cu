2b1e1fbbd2787031743d8f37f25a5e6e5c4f4d98
Wayne Gaudin <wpg@dion.hpsg.dcs.warwick.ac.uk>
2013-03-01 15:08:35 +0000

    added GLPL to cuda files

--- PdV_kernel_cuda.cu
+++ PdV_kernel_cuda.cu
@@ -1,3 +1,23 @@
+!Crown Copyright 2012 AWE.
+!
+! This file is part of CloverLeaf.
+!
+! CloverLeaf is free software: you can redistribute it and/or modify it under
+! the terms of the GNU General Public License as published by the
+! Free Software Foundation, either version 3 of the License, or (at your option)
+! any later version.
+!
+! CloverLeaf is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+! FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+! details.
+!
+! You should have received a copy of the GNU General Public License along with
+! CloverLeaf. If not, see http://www.gnu.org/licenses/.
+
+!>  @brief CloverLeaf top level program: Invokes the main cycle
+!>  @author Michael Boulton
+!>  @details CLoverLeaf in a proxy-app that solves the compressible Euler
 
 #include <iostream>
 #include "cuda_common.cu"
